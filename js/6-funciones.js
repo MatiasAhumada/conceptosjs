8209e5025ac91e3991038d9b1af2eba1265002b8
matias <pagosptcmatias@gmail.com>
2022-07-19 17:47:08 -0300

    agg funciones

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -1,19 +1,45 @@
-//funcion que retorna un valor
-// function convertirDolaresaPesos(dolares) {
-//     let precioFinal= dolares * 301;
+//funcion sin parametros
+function saludar(){
+    //todo el codigo que quiero que tenga esta funcion
+    document.write('<p>Hola mundo</p>')
+};
+
+//funcion con parametro
+function saludoNuevo(nombre, apellido){
+    document.write(`<p>Hola mi nombre es ${nombre}, mi apellido es ${apellido}</p>`)
+}
+
+
+//funciones que retornan un valor
+// function convertirDolaresAPesos(dolares){
+//     let precioFinal = dolares * 301;
 //     return precioFinal;
 // }
 
-// let convertirDolaresaPesos = ( dolares) => {
-//     let precioFinal= dolares * 301;
+// let convertirDolaresAPesos = (dolares) =>{
+//     let precioFinal = dolares * 301;
 //     return precioFinal;
 // }
 
-let convertirDolaresaPesos = dolares => dolares * 301
+let convertirDolaresAPesos = dolares => dolares * 301;
 
-// invocar o llamar a una funcion 
+
+//invocar o llamar a un funcion
 saludar();
- let nombre = prompt('ingrese un sombre')
- let apellido = prompt('ingrese un apellido')
 
- saludoNuevo(nombre, apellido);
+let nombre = prompt('Ingrese un nombre');
+let apellido = prompt('Ingrese un apellido');
+
+saludoNuevo(nombre, apellido);
+
+let play = convertirDolaresAPesos(800);
+console.log(play);
+
+document.write(`<p>AMD Ryzen 9 $${convertirDolaresAPesos(550)} sin impuestos.</p>`)
+
+
+
+
+
+
+
