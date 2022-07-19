d31e26e29f353f8c20da610acf7e43ef8ad54bb8
matias <pagosptcmatias@gmail.com>
2022-07-19 17:39:57 -0300

    agg

---
+++ js/6-funciones.js
@@ -0,0 +1,19 @@
+//funcion que retorna un valor
+// function convertirDolaresaPesos(dolares) {
+//     let precioFinal= dolares * 301;
+//     return precioFinal;
+// }
+
+// let convertirDolaresaPesos = ( dolares) => {
+//     let precioFinal= dolares * 301;
+//     return precioFinal;
+// }
+
+let convertirDolaresaPesos = dolares => dolares * 301
+
+// invocar o llamar a una funcion 
+saludar();
+ let nombre = prompt('ingrese un sombre')
+ let apellido = prompt('ingrese un apellido')
+
+ saludoNuevo(nombre, apellido);
